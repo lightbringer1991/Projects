56694f116b7fad0bbfb338827d1f159ef3aa8abf
lightbringer1991 <lightbringer1991@yahoo.com>
2016-01-15 00:17:10 +1100

    perfected the loading calculation

--- PHP_Process_Data_From_MySQL/lib/Beam.class.php
+++ PHP_Process_Data_From_MySQL/lib/Beam.class.php
@@ -129,7 +129,6 @@ class Beam {
 			$e = new Element($startNode, $endNode, $pk4ba_mat, $pk4ba_g);
 			$startNode -> elementStart = $e;
 			$endNode -> elementEnd = $e;
-			// $this -> elements[] = $e;
 		}
 	}
 
@@ -154,18 +153,21 @@ class Beam {
 				$startNode = new Node($loadingData['startLocation'], 0, 0);
 				$endNode = new Node($loadingData['endLocation'], 0, 0);
 				$nodeList = $this -> getAllNodesBetweenNodes($startNode, $endNode);
-				// echo '<pre>';
-				// print_r($nodeList);
-				// echo '</pre>';
-				// $vList = $this -> calculateLoading($nodeList, $loadingData['startValue'], $loadingData['endValue']);
-				$this -> calculateLoading($nodeList, $loadingData['startValue'], $loadingData['endValue']);
-				// for ($i = 0; $i < count($nodeList); $i++) {
-				// 	if ($loadingData['type'] == 'Force') {
-				// 		$nodeList[$i] -> loading -> fz = $vList[$i];
-				// 	} elseif ($loadingData['type'] == 'Moment') {
-				// 		$nodeList[$i] -> loading -> my = $vList[$i];
-				// 	}
-				// }
+
+				$vList = $this -> calculateLoading($nodeList, $loadingData['startValue'], $loadingData['endValue']);
+				for ($i = 0; $i < count($nodeList); $i++) {
+					if ($loadingData['type'] == 'Force') {
+						if ($nodeList[$i] -> loading -> fz == null) {
+							$nodeList[$i] -> loading -> fz = 0;
+						}
+						$nodeList[$i] -> loading -> fz += $vList[$i];
+					} elseif ($loadingData['type'] == 'Moment') {
+						if ($nodeList[$i] -> loading -> my == null) {
+							$nodeList[$i] -> loading -> my = 0;
+						}
+						$nodeList[$i] -> loading -> my += $vList[$i];
+					}
+				}
 			}
 		}
 	}
@@ -199,62 +201,72 @@ class Beam {
 
 	// assuming $nodeList is sorted in ascending order
 	private function calculateLoading($nodeList, $fs, $fe) {
-		// $totalLength = new Element($nodeList[0], $nodeList[count($nodeList) - 1], 0, 0);
-		// $totalLength = $totalLength -> getLength();
-
 		// get distance of each node to its neighbors
 		$eArray = array();
 		$totalLength = 0;
 		for ($i = 1; $i < count($nodeList); $i++) {
 			$eArray[$i - 1] = $nodeList[$i] -> elementEnd -> getLength();
 			$totalLength += $nodeList[$i] -> elementEnd -> getLength();
-			// print $nodeList[$i] -> elementEnd -> startNode -> x . " start\n";
-			// print $nodeList[$i] -> x . " $$$$\n";
-			// print $nodeList[$i] -> elementEnd -> getLength() . "\n";
 		}
-		// print $totalLength . " Total\n";
 
 		// get all f for each node
 		$fArray = array($fs);
-		print "$fs  fstart\n";
-		print "$fe  fend\n";
-		print "$totalLength Total\n";
-		$elie = $this -> sumArray($eArray, 1, 1);
-		print $elie . "\n";
-		print ($fe - $fs) / $totalLength * $elie . "\n";
-		print $fs + ($fe - $fs) / $totalLength * $elie . "\n";
-		// $eArray = array();
 		for ($i = 1; $i < count($nodeList) - 1; $i++) {
-			// if ($nodeList[$i] -> value != null) { $fArray[$i] = $nodeList[$i] -> value; }
-			$fArray[$i] = $fs + ($fe - $fs) / $totalLength * $this -> sumArray($eArray, $i, count($eArray) - 1);
-
-			// // element between end node and node i
-			// $elsi = new Element($nodeList[$i], $nodeList[count($nodeList) - 1], 0, 0);
-			// $eArray[$i - 1] = $elsi;
-			// $fArray[$i] = $fe + ($fs - $fe) / $totalLength * $eArray[$i - 1] -> getLength();				
+			$fArray[$i] = $fs + ($fe - $fs) / $totalLength * $this -> sumArray($eArray, 0, $i - 1);
 		}
 		// generate last element
-		$eArray[] = new Element($nodeList[count($nodeList) - 2], $nodeList[count($nodeList) - 1], 0, 0);
 		$fArray[] = $fe;
-		print var_dump($fArray);
-		
-		// // calculate all v for each node
-		// // calculate start node value
-		// $f12 = ($fArray[0] - $fArray[1]) / 2;
-		// $vs = ($fArray[0] + $f12) * $eArray[0] -> getLength() / 2;
-		// $vArray = array(number_format($vs, 2));
-		// for ($i = 1; $i < count($nodeList) - 1; $i++) {
-		// 	// calculate f(i, i+1), e.g. f12, f23, f34, etc.
-		// 	$fi = $fArray[$i] + ($fArray[$i - 1] - $fArray[$i]) / 2;
-		// 	$fi1 = $fArray[$i + 1] + ($fArray[$i] - $fArray[$i + 1]) / 2;
-		// 	$vArray[$i] = number_format(($fi + $fArray[$i]) * $eArray[$i - 1] -> getLength() + ($fArray[$i] + $fi1) * $eArray[$i] -> getLength(), 2);
-		// }
-		// // calculate v end
-		// $fxe = ($fArray[count($fArray) - 2] - $fArray[count($fArray) - 1]) / 2;
-		// $ve = ($fxe + $fArray[count($fArray) - 1]) * $eArray[count($eArray) - 1] -> getLength() / 2;
-		// $vArray[] = number_format($ve, 2);
 
-		// return $vArray;
+		// check if there is a node with 0 value
+		// if there is, split the array into 2, recursively run calculateLoading() again with 2 sections
+		$zeroIndex = -1;
+		for ($i = 0; $i < count($fArray); $i++) {
+			if ($fArray[$i] < Node::$delta) {
+				$zeroIndex = $i;
+				break;
+			}
+		}
+		if ( ($zeroIndex != -1) && ($zeroIndex != 0) && ($zeroIndex != count($nodeList) - 1) ) {
+			print $zeroIndex;
+			$arr1 = array();
+			$arr2 = array($nodeList[$zeroIndex]);
+			for ($i = 0; $i < count($nodeList); $i++) {
+				if ($i <= $zeroIndex) {
+					array_push($arr1, $nodeList[$i]);
+				} else {
+					array_push($arr2, $nodeList[$i]);
+				}
+			}
+			$vArray1 = $this -> calculateLoading($arr1, $fs, 0);
+			$vArray2 = $this -> calculateLoading($arr2, 0, $fe);
+			print var_dump($vArray1);
+			print var_dump($vArray2);
+			// unshift the first index of $vArray2, as it will be the zero node, add to $arr1
+			$vArray2zero = array_shift($vArray2);
+			$vArray1[count($vArray1) - 1] += $vArray2zero;
+			return array_merge($vArray1, $vArray2);
+		}
+
+		// generate V at all nodes
+		// calculate V at start node
+		$vs = $fArray[1] * $eArray[0] / 2 + ($fArray[0] - $fArray[1])  * $eArray[0] / 3;
+		$vArray = array($vs);
+		// generate V at all nodes except the last node
+		for ($i = 1; $i < count($nodeList) - 1; $i++) {
+			$A = $fArray[$i] * $eArray[$i - 1] / 2;
+			$B = ($fArray[$i - 1] - $fArray[$i]) * $eArray[$i - 1] / 3;
+			$C = $fArray[$i + 1] * $eArray[$i] / 2;
+			$D = ($fArray[$i] - $fArray[$i + 1]) * $eArray[$i] / 2;
+
+			$vi = $A + $B / 3 + $C + $D * 2 / 3;
+			array_push($vArray, $vi);
+		}
+		// calculate V at end node
+		$endIndex = count($nodeList) - 1;
+		$ve = $fArray[$endIndex] * $eArray[$endIndex - 1] / 2 + ($fArray[$endIndex - 1] - $fArray[$endIndex]) * $eArray[$endIndex - 1] / 6;
+		array_push($vArray, $ve);
+		
+		return $vArray;
 	}
 
 	private function sumArray($arr, $start, $end) {
